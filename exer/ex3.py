52b813b3879c45e13626085f321ceb76dc7f34bd
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-03-01 01:27:32 -0300

    ex3
    Trabalhando com numeros inteiros

---
+++ exer/ex3.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = n1 + n2
+m = n1 - n2
+d = n1 / n2
+v = n1 * n2
+
+print('Somar: ', s)
+print('Menos: ', m)
+print ('Dividir: ', d)
+print ('Vezes: ', v)
